c2d67d104cefe8f6458872036ddb2b5c8e954bd9
Bariss26 <bariskokuluiki@gmail.com>
2023-06-13 02:51:44 +0300

    presence and error display

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import 'dotenv/config'
 import { ActivityType, Client } from 'discord.js';
 
-const client = new Client({intents:['Guilds', 'GuildMessages', 'MessageContent']});
+const client = new Client({ intents: ['Guilds', 'GuildMessages', 'MessageContent'] });
 
 client.login(process.env.TOKEN);
 
 client.on('ready', () => {
-    client.user.setPresence({activities: [{name: ' the handle.', type: ActivityType.Watching}]})
+    let flip = 0;
+    setInterval(() => {
+        switch (flip) {
+            case 0:
+                client.user.setPresence({ activities: [{ name: 'the handle.', type: ActivityType.Watching }] })
+                flip++;
+                break;
+            case 1:
+                client.user.setPresence({ activities: [{ name: 'ej', type: ActivityType.Listening }] })
+                flip = 0;
+                break;
+        }
+    }, 5000)
 });
 
-client.on('messageCreate', (msg) => {
-    if(msg.content=="ej") {
-        msg.member.kick();
-        msg.reply("Ejected!");
-    }
+client.on('messageCreate', async (msg) => {
+    try {
+        if (msg.content == "ej") {
+            msg.member.kick().then((m) => {
+                msg.reply("Ejected!");
+            }).catch((e) => {
+                msg.reply(`Teehee ${e.stack.split("\n")[0]}`);
+            });
+        }
+    } catch (ee) { };
 });
